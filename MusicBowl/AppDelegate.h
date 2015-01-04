8835f41383ac491b080e7bcad6c04f1f788191c8
Jeremy <jw2016@mit.edu>
2015-01-04 16:22:55 +0100

    Initial Commit

---
+++ MusicBowl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicBowl
+//
+//  Created by Jeremy Wohlwend on 1/4/15.
+//  Copyright (c) 2015 jwohlwend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicBowl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MusicBowl
+//
+//  Created by Jeremy Wohlwend on 1/4/15.
+//  Copyright (c) 2015 jwohlwend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
